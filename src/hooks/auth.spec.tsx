aa1a43c1c820f2a28c9d97878cf90dd08aed81c1
Pcfilho <paulofilhobarroso@gmail.com>
2023-01-31 00:10:43 -0300

    Hooks and mocks tests

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,15 +1,38 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
 
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => {
+      return {
+        type: 'success',
+        params: {
+          access_token: 'google_token',
+        }
+      }
+    }
+  }
+})
 
 describe('Auth Hook', () => {
 
-  it('should be able to sign in with Google account existing', () => {
+  it('should be able to sign in with Google account existing', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve({
+        id: `userInfo.id`,
+        email: `userInfo.email`,
+        name: `userInfo.given_name`,
+        photo: `userInfo.picture`,
+        locale: `userInfo.locale`,
+        verified_email: `userInfo.verified_email`,
+      }),
+    })) as jest.Mock;
+
       const { result } = renderHook(() => useAuth(), {
         wrapper: AuthProvider
       });
 
-      result.current.signInWithGoogle();
+      await act(() => result.current.signInWithGoogle())
 
       expect(result.current.user).toBeTruthy();
   })
