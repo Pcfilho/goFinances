21d751004ff4c2871dd5e2db55148e3593240724
Paulo <paulofilhobarroso@gmail.com>
2023-02-08 13:29:49 -0300

    Adding coverage collector to project.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,31 @@
 const config = {
     preset: "jest-expo",
+
     testPathIgnorePatterns: [
         '/node_modules',
         '/android',
         '/ios'
     ],
+
     setupFilesAfterEnv: [
         "@testing-library/jest-native/extend-expect",
         "jest-styled-components"
     ],
+
     transformIgnorePatterns: [
         "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|react-native-responsive-fontsize|native-base|react-native-svg)"
     ],
-    setupFiles: ["./jestSetupFile.js"]
+
+    setupFiles: ["./jestSetupFile.js"],
+
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.tsx",
+        "!src/**/*.spec.tsx",
+    ],
+    coverageReporters: [
+        "lcov"
+    ]
 }
 
 module.exports = config
